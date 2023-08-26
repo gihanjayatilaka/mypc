20b5c3bf9e6096fa793c6bafa4b4d79e59a552a0
Gihan Jayatilaka <gihan@umd.edu>
2023-08-26 19:18:37 -0400

    Update tmuxRightScreen.sh

--- scripts/tmuxRightScreen.sh
+++ scripts/tmuxRightScreen.sh
@@ -1,4 +1,9 @@
 # This shell script splits the portrait monitor (usually, right side) into (1) terminal (2) htop and (3) nvitop.
+# This script requires tmux (https://anaconda.org/conda-forge/tmux)
+#                      htop (https://anaconda.org/conda-forge/htop) and 
+#                      nvitop (https://github.com/XuehaiPan/nvitop#installation) installed inside the conda environment
+
+
 CONDA_ENV="myCondaEnv"
 
 source ~/.bashrc
